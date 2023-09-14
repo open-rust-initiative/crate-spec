1bb54e02beab5f431fafdf44aad0b9f07c349d6f
shuibing <191098191@smail.nju.edu.cn>
2023-09-14 20:03:16 +0800

    [utils/pkcs] pkcs7 lib and test ok

--- crate-spec/src/utils/pkcs.rs
+++ crate-spec/src/utils/pkcs.rs
@@ -1,11 +1,98 @@
+use std::fs;
+use std::path::Path;
+use openssl::hash::{hash, MessageDigest};
+use openssl::nid::Nid;
 use openssl::pkcs7::Pkcs7;
 use openssl::pkcs7::Pkcs7Flags;
 use openssl::pkey::PKey;
 use openssl::stack::Stack;
 use openssl::x509::store::X509StoreBuilder;
 use openssl::x509::X509;
+
+
+pub struct PKCS{
+    cert_bin: Vec<u8>,
+    pkey_bin: Vec<u8>,
+    root_ca_bin: Vec<u8>
+}
+
+impl PKCS{
+    pub fn new()->Self{
+        Self{
+            cert_bin: vec![],
+            pkey_bin: vec![],
+            root_ca_bin: vec![],
+        }
+    }
+    pub fn load_from_file(&mut self, cert_path: String, pkey_path: String, ca_path: String){
+        //just for demo
+        self.cert_bin = fs::read(Path::new(cert_path.as_str())).unwrap();
+        self.pkey_bin = fs::read(Path::new(pkey_path.as_str())).unwrap();
+        self.root_ca_bin = fs::read(Path::new(ca_path.as_str())).unwrap();
+    }
+
+    pub fn encode_pkcs_bin(&self, message:&[u8])->Vec<u8>{
+        let cert = X509::from_pem(self.cert_bin.as_slice()).unwrap();
+        let certs = Stack::new().unwrap();
+        let flags = Pkcs7Flags::STREAM;
+        let pkey = PKey::private_key_from_pem(self.pkey_bin.as_slice()).unwrap();
+        let mut store_builder = X509StoreBuilder::new().expect("should succeed");
+
+        let root_ca = X509::from_pem(self.root_ca_bin.as_slice()).unwrap();
+        store_builder.add_cert(root_ca).expect("should succeed");
+
+        let store = store_builder.build();
+
+        let pkcs7 =
+            Pkcs7::sign(&cert, &pkey, &certs, message, flags).expect("should succeed");
+
+        let signed = pkcs7
+            .to_smime(message, flags)
+            .expect("should succeed");
+        signed
+    }
+
+    pub fn decode_pkcs_bin(&self, signed_bin:&[u8])->Vec<u8>{
+        let cert = X509::from_pem(self.cert_bin.as_slice()).unwrap();
+        let certs = Stack::new().unwrap();
+        let flags = Pkcs7Flags::STREAM;
+        let pkey = PKey::private_key_from_pem(self.pkey_bin.as_slice()).unwrap();
+        let mut store_builder = X509StoreBuilder::new().expect("should succeed");
+
+        let root_ca = X509::from_pem(self.root_ca_bin.as_slice()).unwrap();
+        store_builder.add_cert(root_ca).expect("should succeed");
+
+        let store = store_builder.build();
+
+        let (pkcs7_decoded, content) =
+            Pkcs7::from_smime(signed_bin).expect("should succeed");
+
+        let mut output = Vec::new();
+        pkcs7_decoded
+            .verify(&certs, &store, None, Some(&mut output), flags)
+            .expect("should succeed");
+        output
+    }
+
+    pub fn gen_digest_256(&self, bin:&[u8])->Vec<u8>{
+        let res = hash(MessageDigest::sha256(), bin).unwrap();
+        res.to_vec()
+    }
+}
+
 #[test]
-fn test_pkcs(){
+fn test_PKCS(){
+    let mut pkcs = PKCS::new();
+    pkcs.load_from_file("test/cert.pem".to_string(), "test/key.pem".to_string(), "test/root-ca.pem".to_string());
+    let bin = "Hello rust!".to_string();
+    let digest = pkcs.gen_digest_256(bin.as_bytes());
+    let signedData = pkcs.encode_pkcs_bin(digest.as_slice());
+    let digest_de = pkcs.decode_pkcs_bin(signedData.as_slice());
+    assert_eq!(digest, digest_de);
+}
+
+#[test]
+fn test_pkcs7(){
     let cert = include_bytes!("../../test/cert.pem");
     let cert = X509::from_pem(cert).unwrap();
     let certs = Stack::new().unwrap();
@@ -38,4 +125,16 @@ fn test_pkcs(){
 
     assert_eq!(output, message.as_bytes());
     assert!(content.is_none());
+}
+
+#[test]
+fn test_hash() -> Result<(), Box<dyn std::error::Error>> {
+    use openssl::hash::{hash, MessageDigest};
+
+    let data = b"\x42\xF4\x97\xE0";
+    //let spec = b"\x7c\x43\x0f\x17\x8a\xef\xdf\x14\x87\xfe\xe7\x14\x4e\x96\x41\xe2";
+    let res = hash(MessageDigest::sha256(), data)?;
+    println!("{:?}", &*res);
+    //assert_eq!(&*res, spec);
+    Ok(())
 }
