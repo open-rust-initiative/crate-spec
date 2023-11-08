3a3419dbaff56239589b925052350836b3274ec2
Bing Shui <62472682+Shbinging@users.noreply.github.com>
2023-11-08 15:30:29 +0800

    fix encode_crate.sh bug

--- crate-spec/test/example/encode_crate.sh
+++ crate-spec/test/example/encode_crate.sh
@@ -1,6 +1,7 @@
 script_dir=$(cd "$(dirname "$0")" && pwd)
 test_dir=$(dirname "$script_dir")
 crate_dir=$(dirname "$test_dir")
+mkdir -p "$test_dir"/output
 cd "$script_dir" || exit
 cargo build || exit
 cargo run -- -e -c "$test_dir"/cert.pem -r "$test_dir"/root-ca.pem -p "$test_dir"/key.pem -o "$test_dir"/output/ "$crate_dir" || exit
